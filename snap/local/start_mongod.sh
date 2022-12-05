710b6cf0fdcd14ed0c9590069bcd705006bda83d
Jarred Wilson <jarred.wilson12@gmail.com>
2022-12-05 15:49:04 -0500

    Configure snap to use /etc/mongo.conf with sane defaults
    Rework CI branch filtering
    
    Change confinement for edge channel
    
    Strictly confine snap
    
    Add layouts to snapcraft
    
    Add config to start script
    
    Rework config file for default behavior

--- snap/local/start_mongod.sh
+++ snap/local/start_mongod.sh
@@ -5,6 +5,7 @@ if [ ! -f "${CONFIG_FILE}" ]; then
         echo "configuration file does not exist."
         echo "copying default config to ${CONFIG_FILE}"
         cp -r $SNAP/etc/mongod.conf $SNAP_COMMON
+	mkdir -p $SNAP_COMMON/db
 fi
 
-$SNAP/usr/bin/mongod "$@"
+$SNAP/usr/bin/mongod --config $CONFIG_FILE "$@"
