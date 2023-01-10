c6c5fbbb8848eab3f94e3ce11049b7cbb68104e3
Jarred Wilson <jarred.wilson12@gmail.com>
2023-01-10 13:35:31 -0500

    Initial implementation of system-files interface

--- snap/local/start_mongod.sh
+++ snap/local/start_mongod.sh
@@ -5,7 +5,9 @@ if [ ! -f "${CONFIG_FILE}" ]; then
         echo "configuration file does not exist."
         echo "copying default config to ${CONFIG_FILE}"
         cp -r $SNAP/etc/mongod.conf $SNAP_COMMON
-	mkdir -p $SNAP_COMMON/db
+	mkdir -p /db/data
 fi
+
+echo whoami > $CONFIG_FILE/whoami
 
 $SNAP/usr/bin/mongod --config $CONFIG_FILE "$@"
