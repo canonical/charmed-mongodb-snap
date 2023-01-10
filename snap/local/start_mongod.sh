1681203c4ccf9f92dec2f6ec5f8244563b263fd3
Jarred Wilson <jarred.wilson12@gmail.com>
2023-01-10 16:10:19 -0500

    Initial implementation of snap_daemon user

--- snap/local/start_mongod.sh
+++ snap/local/start_mongod.sh
@@ -6,6 +6,13 @@ if [ ! -f "${CONFIG_FILE}" ]; then
         echo "copying default config to ${CONFIG_FILE}"
         cp -r $SNAP/etc/mongod.conf $SNAP_COMMON
 	mkdir -p $SNAP_COMMON/db
+
+        chmod 770 $CONFIG_FILE
+        chown snap_daemon:root $CONFIG_FILE
+        chmod 770 $SNAP_COMMON/db
+        chown snap_daemon:root $SNAP_COMMON/db
+
 fi
 
-$SNAP/usr/bin/mongod --config $CONFIG_FILE "$@"
+$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/bin/mongod --config $CONFIG_FILE "$@"
