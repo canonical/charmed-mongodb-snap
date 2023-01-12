d8df684c2378b9660a7383e59263c07ad6069990
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-01-12 11:23:36 -0500

    Drop privileges to snap_daemon user (#2)
    * Initial implementation of snap_daemon user
    
    * Update file permissions for unpriv user
    
    * Fix incorrect db path
    
    * Add comments to start_mongod.sh

--- snap/local/start_mongod.sh
+++ snap/local/start_mongod.sh
@@ -6,6 +6,13 @@ if [ ! -f "${CONFIG_FILE}" ]; then
         echo "copying default config to ${CONFIG_FILE}"
         cp -r $SNAP/etc/mongod.conf $SNAP_COMMON
 	mkdir -p $SNAP_COMMON/db
+	touch $SNAP_DATA/mongod.log
+
+	# Change ownership of snap directories to allow snap_daemon to read/write
+        chown -R snap_daemon:root $SNAP_DATA
+        chown -R snap_daemon:root $SNAP_COMMON
 fi
 
-$SNAP/usr/bin/mongod --config $CONFIG_FILE "$@"
+# For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
+$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/bin/mongod --config $CONFIG_FILE "$@"
