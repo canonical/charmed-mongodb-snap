f7a57c48129425ecaee38a958e2c413348939d26
jarred wilson <20207921+jardon@users.noreply.github.com>
2023-02-08 09:29:14 -0500

    [DPE-1196] Bundle all mongo components in snap (#3)
    * Bundle all mongo components in snap

--- snap/local/start_mongod.sh
+++ snap/local/start_mongod.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-export CONFIG_FILE="${SNAP_COMMON}/mongod.conf"
-if [ ! -f "${CONFIG_FILE}" ]; then
-        echo "configuration file does not exist."
-        echo "copying default config to ${CONFIG_FILE}"
-        cp -r $SNAP/etc/mongod.conf $SNAP_COMMON
-	mkdir -p $SNAP_COMMON/db
-	touch $SNAP_DATA/mongod.log
-
-	# Change ownership of snap directories to allow snap_daemon to read/write
-        chown -R snap_daemon:root $SNAP_DATA
-        chown -R snap_daemon:root $SNAP_COMMON
-fi
-
 # For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/mongod --config $CONFIG_FILE "$@"
+  --regid snap_daemon -- $SNAP/usr/bin/mongod --config ${SNAP_COMMON}/mongod.conf "$@"
