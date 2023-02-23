5e4178cd54cc4486069fc949031d3bea634242f5
Jarred Wilson <jarred.wilson12@gmail.com>
2023-02-23 13:16:16 -0500

    Add exec to mongod wrapper script

--- snap/local/start-mongod.sh
+++ snap/local/start-mongod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
-$SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/mongod --config ${SNAP_COMMON}/mongod.conf "$@"
