b23b68185fb03a8b45cced4a7744d42f0e083c1f
Mia Altieri <mgaltier200@gmail.com>
2023-03-07 08:02:36 +0000

    update conf files according to spec

--- snap/local/start-mongod.sh
+++ snap/local/start-mongod.sh
@@ -2,4 +2,4 @@
 
 # For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/mongod --config ${SNAP_COMMON}/mongod.conf ${MONGOD_ARGS} "$@"
+  --regid snap_daemon -- $SNAP/usr/bin/mongod --config "${SNAP_DATA}/etc/mongod/mongod.conf" ${MONGOD_ARGS} "$@"
