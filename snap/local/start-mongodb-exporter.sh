6c9ccb5b8562055014698a19f29c896f427d5f43
Mia Altieri <32723809+MiaAltieri@users.noreply.github.com>
2023-03-14 16:59:47 +0100

    Add mongodb exporter (#14)
    * update conf files according to spec
    
    * update data directory according to spec
    
    * update log directory according to spec
    
    * update pid directory according to spec
    
    * create necessary directories
    
    * minor fixes
    
    * create necessary sub dir
    
    * pr comments
    
    * layout not needed
    
    * add mongodb exporter

---
+++ snap/local/start-mongodb-exporter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: 
+# snap-daemon and use the configured value for the uri
+exec "${SNAP}"/usr/bin/setpriv \
+        --clear-groups \
+        --reuid snap_daemon \
+        --regid snap_daemon -- \
+        "$SNAP/usr/bin/mongodb_exporter" --mongodb-uri "$(snapctl get uri)"
+
