0f7bc6fe553eab9127d1da687d1e3cd76962ca8f
Jarred Wilson <jarred.wilson12@gmail.com>
2023-03-23 11:36:19 -0400

    Fix linting

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -37,7 +37,7 @@ def test_all_services():
         snapcraft = yaml.safe_load(file)
 
         skip = []
-        
+
         for app, data in snapcraft["apps"].items():
             if bool(data.get("daemon")) and app not in skip:
                 print(f"\nTesting {snapcraft['name']}.{app} service....")
