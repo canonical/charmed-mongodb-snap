9df9c4672c6a3c916cdaab8d2f0dfe2da5e63452
Mia Altieri <mgaltier200@gmail.com>
2023-09-13 12:30:01 +0000

    hide config uri

--- snap/local/start-mongos.sh
+++ snap/local/start-mongos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # For security measures, daemons should not be run as sudo. Execute mongos as the non-sudo user: snap-daemon.
+export CONFIG_SERVER_URI="$(snapctl get config-uri)"
+
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/mongos ${MONGOS_ARGS} "$@"
+  --regid snap_daemon -- $SNAP/usr/bin/mongos ${MONGOS_ARGS} --configdb $CONFIG_SERVER_URI "$@"
   
    
