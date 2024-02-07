090b08019bf39839329a1a0e3f8b2d2678f6e0b3
Dmitry Ratushnyy <dmitry.ratushnyy@canonical.com>
2024-02-07 12:53:29 +0000

    Revert command for starting mongod

--- snap/local/start-mongod.sh
+++ snap/local/start-mongod.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
 # For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
-exec \
-  "${SNAP}/usr/bin/setpriv" \
-  --clear-groups \
-  --reuid snap_daemon \
-  --regid snap_daemon \
-  -- \
-  "${SNAP}/usr/bin/mongod" \
-      --config "${SNAP_DATA}/etc/mongod/mongod.conf" \
-      --setParameter processUmask=037 \
-      "${MONGOD_ARGS}" \
-      "$@"
+exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/bin/mongod --config ${SNAP_DATA}/etc/mongod/mongod.conf ${MONGOD_ARGS} "$@"
