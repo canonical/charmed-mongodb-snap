b34aa57393c05bbbffec26c2d35cda998abd360f
Mykola Marzhan <mykola.marzhan@canonical.com>
2024-03-13 17:50:26 +0100

    fix ulimit
    recommended values https://www.mongodb.com/docs/manual/reference/ulimit/

--- snap/local/start-mongos.sh
+++ snap/local/start-mongos.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+ulimit -SHf unlimited
+ulimit -SHt unlimited
+ulimit -SHv unlimited
+ulimit -SHm unlimited
+ulimit -Sl unlimited
+ulimit -SHn 64000
+ulimit -SHu 64000
+
 # For security measures, daemons should not be run as sudo. Execute mongos as the non-sudo user: snap-daemon.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/mongos ${MONGOS_ARGS} "$@"
-  
-   
