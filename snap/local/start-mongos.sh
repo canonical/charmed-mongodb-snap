cb559ee0e81b2caf6f7a9e804861cc57c814144e
Neha Oudin <neha@oudin.red>
2024-11-22 11:15:43 +0100

    feat: Use snap config for starting services
    This is backward compatible.

--- snap/local/start-mongos.sh
+++ snap/local/start-mongos.sh
@@ -8,6 +8,12 @@ ulimit -Sl unlimited
 ulimit -SHn 64000
 ulimit -SHu 64000
 
+SNAP_ARGS="$(snapctl get mongos-args)"
+
+if [[ -n "${SNAP_ARGS}" ]]; then
+    MONGOS_ARGS=$SNAP_ARGS
+fi
+
 # For security measures, daemons should not be run as sudo. Execute mongos as the non-sudo user: snap-daemon.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/mongos ${MONGOS_ARGS} "$@"
