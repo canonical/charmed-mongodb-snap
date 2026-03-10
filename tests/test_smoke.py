91de94ceeb999d727d75221f8628721781b4a1ab
Neha Oudin <neha@oudin.red>
2026-03-10 16:05:55 +0100

    feat(DPE-9339): Add vault & mongod-cli to snap

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -80,6 +80,7 @@ def test_refresh():
         Path("/var/snap/charmed-mongodb/current/etc/mongod/mongod.conf"),
         Path("/var/snap/charmed-mongodb/current/etc/mongod/mongos.conf"),
         Path("/var/snap/charmed-mongodb/current/etc/ldap/ldap.conf"),
+        Path("/var/snap/charmed-mongodb/current/etc/vault/vault.hcl"),
     ]
 
     for file_path in mandatory_files:
